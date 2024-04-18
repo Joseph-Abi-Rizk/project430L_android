732823839ab2b732add37dbc45cb54937ef9afd4
Joseph-Abi-Rizk <jna31@mail.aub.edu>
2024-04-18 16:51:22 +0300

    recycle

--- app/src/main/java/com/josephabirizk/currencyexchange/api/model/Token.kt
+++
@@ -1,7 +0,0 @@
-package com.josephabirizk.currencyexchange.api.model
-
-import com.google.gson.annotations.SerializedName
-class Token {
-    @SerializedName("token")
-    var token: String? = null
-}
